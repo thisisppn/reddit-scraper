6d6cd1421a1dc22a6503e52c2a762fe8cc9a9bee
thisisppn <thisisppn@gmail.com>
2015-06-23 01:09:27 +0530

    Added function to download gifs as mp4 from gyfcat and also download photos from imgur.com albums

--- reddit.js
+++ reddit.js
@@ -6,6 +6,7 @@ var next_page = [];
 var jar = request.jar();
 var images = [];
 var i = 0;
+var directory = "testing/";
 
 var cookie = request.cookie("over18=1");
 cookie.domain = "reddit.com";
@@ -25,12 +26,7 @@ request({
     method: "GET",
     jar: jar
 }, function(error, response, body) {
-    // if (error) {
-    // 	console.log(error);
-    // 	var retry = next_page[(next_page.length)-1];
-    // 	scrape(retry);
-    // };
-
+   
     if (!error) {
     	$ = cheerio.load(body);
     
@@ -53,6 +49,7 @@ request({
 
 	$('a.title').each(function(index, links){
 		var URL = $(links).attr("href");
+		// console.log(URL);
 		if (URL.indexOf("i.imgur.com")!= "-1") {
 			console.log(URL);
 			if (URL.indexOf(".jpg")!= "-1") {
@@ -65,11 +62,15 @@ request({
 				var filename = Math.floor((Math.random() * 1000000) + 1)+".gif";	
 			}
 
-			download(URL, "nsfw/"+filename, function(){
+			download(URL, directory+filename, function(){
 				console.log("Done");
 				i++;
 				console.log(i);
 			});
+		}else if(URL.indexOf("gfycat.com")!= "-1"){
+			findVideoLink(URL);
+		}else if (URL.indexOf("imgur.com")==7) {
+			scrape_imgurl(URL);
 		};
 	});
 
@@ -90,7 +91,7 @@ var download = function(uri, filename, callback){
     // console.log('content-type:', res.headers['content-type']);
     // console.log('content-length:', res.headers['content-length']);
     // request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-
+-
 	request
 	  .get(uri)
 	  .on('error', function(err) {
@@ -107,13 +108,55 @@ var download = function(uri, filename, callback){
 //Function to scrape image from imgurl.com
 
 var scrape_imgurl = function(target_imgurl){
+	var link;
+	console.log("TARGET URL"+target_imgurl);
+	request(target_imgurl, function(err, res, body){
+		if (!err) {
+			$ = cheerio.load(body);
+			$(".image img").each(function(index, image){
+				link = $(image).attr("src");
+				console.log("FOUND IMAGE LINK "+link);
+				if (link.indexOf('i.imgur.com')!= "-1") {
 
-	//Under construction
+						if (link.indexOf(".jpg")!= "-1") {
+							var filename = Math.floor((Math.random() * 1000000) + 1)+".jpg";
+						}else if (link.indexOf(".png")!= "-1") {
+							var filename = Math.floor((Math.random() * 1000000) + 1)+".png";	
+						}else if (link.indexOf(".jpeg")!= "-1") {
+							var filename = Math.floor((Math.random() * 1000000) + 1)+".jpeg";	
+						}else if (link.indexOf(".gif")!= "-1") {
+							var filename = Math.floor((Math.random() * 1000000) + 1)+".gif";	
+						}
+
+						console.log("DOWNLOADING "+link);
+						download("http:"+link, directory+filename, function(){
+							console.log("Done From album");
+						});
+					}
+				});
+		};
+	});
+	
 
 }
 
 
-
+var findVideoLink = function(target_gyf){
+	var link
+	request(target_gyf, function(err, res, body){
+		if (!err) {
+		$ = cheerio.load(body);
+		link = "http:"+$('#mp4source').attr("src");
+		// link = link.replace("//", "http://www.");
+		console.log(link);
+		var filename = Math.floor((Math.random() * 1000000) + 1)+".mp4";
+		download(link, directory+filename, function(){
+			console.log("Done mp4");
+		})
+	};
+	});
+	
+}
 
 
 
